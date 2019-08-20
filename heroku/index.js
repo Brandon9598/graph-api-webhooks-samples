0eb168eedc924221bb54e61b6c65456d1671a58f
Brandon Hancock <brandon9598@gmail.com>
2019-08-20 19:29:15 -0400

    expected signature

--- heroku/index.js
+++ heroku/index.js
@@ -30,6 +30,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
+  console.log('Expected Signature', req.headers['x-hub-signature']);
 
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
