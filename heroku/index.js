0a77e6fa3759bef02629d5048c8b17797ade428e
Brandon Hancock <brandon9598@gmail.com>
2019-08-20 19:27:12 -0400

    index.js update

--- heroku/index.js
+++ heroku/index.js
@@ -1,11 +1,3 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
@@ -21,6 +13,7 @@ var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
+  console.log(req);
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
@@ -44,6 +37,7 @@ app.post('/facebook', function(req, res) {
     return;
   }
 
+  console.log('request header X-Hub-Signature validated');
   // Process the Facebook updates here
   received_updates.unshift(req.body);
   res.sendStatus(200);
