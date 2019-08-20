8db12d5d6deff8bc1218201817f07c33bd43bd56
Brandon Hancock <brandon9598@gmail.com>
2019-08-20 19:40:46 -0400

    testing expcted vs calculated

--- heroku/index.js
+++ heroku/index.js
@@ -2,6 +2,7 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var xhub = require('express-x-hub');
+var crypto = require('crypto');
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
@@ -11,6 +12,12 @@ app.use(bodyParser.json());
 
 var token = process.env.TOKEN || 'token';
 var received_updates = [];
+
+function getSignature(buf) {
+  var hmac = crypto.createHmac("sha1", process.env.APP_SECRET);
+  hmac.update(buf, "utf-8");
+  return "sha1=" + hmac.digest("hex");
+}
 
 app.get('/', function(req, res) {
   console.log(req);
@@ -31,7 +38,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 app.post('/facebook', function(req, res, buf) {
   console.log('Facebook request body:', req.body);
   console.log('Expected Signature', req.headers['x-hub-signature']);
-  console.log('buf', buf.toString('utf8'))
+  console.log('calculated', getSignature(buf))
 
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
